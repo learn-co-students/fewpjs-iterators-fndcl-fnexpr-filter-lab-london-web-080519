067e94e1a903923ddf448ec0a25ba4597277b686
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-09-23 09:05:54 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+let findMatching = (arr, str) => {
+  return arr.filter(item => {
+    return item.toLowerCase() == str.toLowerCase();
+  });
+};
+
+let fuzzyMatch = (arr, str) => {
+  return arr.filter(item => {
+    return item.slice(0, str.length).toLowerCase() == str.toLowerCase();
+  });
+};
+
+let matchName = (arr, str) => {
+  return arr.filter(item => {
+    return item.name == str;
+  });
+};
